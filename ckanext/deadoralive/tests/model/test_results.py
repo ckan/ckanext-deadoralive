e9d45b6a015f9c30db6876b69d2c5e4a45bf7afb
Sean Hammond <git@seanh.cc>
2014-09-15 15:17:17 +0000

    Rename link_checker_results -> results

--- ckanext/deadoralive/tests/model/test_results.py
+++ ckanext/deadoralive/tests/model/test_results.py
@@ -1,4 +1,4 @@
-"""Tests for model/link_checker_results.py."""
+"""Tests for model/results.py."""
 import datetime
 
 import nose.tools
@@ -6,23 +6,23 @@ import nose.tools
 import ckan.new_tests.helpers as helpers
 import ckan.new_tests.factories as factories
 
-import ckanext.deadoralive.model.link_checker_results as link_checker_results
+import ckanext.deadoralive.model.results as results
 
 
 class TestUpsertAndGet(object):
     """Tests for the upsert() and get() functions."""
     def setup(self):
         helpers.reset_db()
-        link_checker_results.create_database_table()
+        results.create_database_table()
 
     def test_insert_successful_result(self):
         """Test checking a resource for the first time when the link is working.
 
         """
         before = datetime.datetime.utcnow()
-        link_checker_results.upsert("test_resource_id", True)
+        results.upsert("test_resource_id", True)
         after = datetime.datetime.utcnow()
-        result = link_checker_results.get("test_resource_id")
+        result = results.get("test_resource_id")
         assert result["resource_id"] == "test_resource_id"
         assert result["alive"] is True
         assert result["last_checked"] > before
@@ -38,9 +38,9 @@ class TestUpsertAndGet(object):
 
         """
         before = datetime.datetime.utcnow()
-        link_checker_results.upsert("test_resource_id", False)
+        results.upsert("test_resource_id", False)
         after = datetime.datetime.utcnow()
-        result = link_checker_results.get("test_resource_id")
+        result = results.get("test_resource_id")
         assert result["resource_id"] == "test_resource_id"
         assert result["alive"] is False
         assert result["last_checked"] > before
@@ -52,13 +52,13 @@ class TestUpsertAndGet(object):
 
     def test_update_with_successful_result(self):
         """Test updating a resource's row with a new successful result."""
-        link_checker_results.upsert("test_resource_id", False)
+        results.upsert("test_resource_id", False)
         before = datetime.datetime.utcnow()
 
-        link_checker_results.upsert("test_resource_id", True)
+        results.upsert("test_resource_id", True)
         after = datetime.datetime.utcnow()
 
-        result = link_checker_results.get("test_resource_id")
+        result = results.get("test_resource_id")
         assert result["resource_id"] == "test_resource_id"
         assert result["alive"] is True
         assert result["last_checked"] > before
@@ -71,13 +71,13 @@ class TestUpsertAndGet(object):
 
     def test_update_with_failed_result(self):
         """Test updating a resource's row with a new failed result."""
-        link_checker_results.upsert("test_resource_id", True)
+        results.upsert("test_resource_id", True)
 
         before = datetime.datetime.utcnow()
-        link_checker_results.upsert("test_resource_id", False)
+        results.upsert("test_resource_id", False)
         after = datetime.datetime.utcnow()
 
-        result = link_checker_results.get("test_resource_id")
+        result = results.get("test_resource_id")
         assert result["resource_id"] == "test_resource_id"
         assert result["alive"] is False
         assert result["last_checked"] > before
@@ -90,13 +90,13 @@ class TestUpsertAndGet(object):
     def test_incrementing_num_fails(self):
         """Test that repeated bad results increment num_fails."""
 
-        link_checker_results.upsert("test_resource_id", False)
-        link_checker_results.upsert("test_resource_id", False)
+        results.upsert("test_resource_id", False)
+        results.upsert("test_resource_id", False)
         before = datetime.datetime.utcnow()
-        link_checker_results.upsert("test_resource_id", False)
+        results.upsert("test_resource_id", False)
         after = datetime.datetime.utcnow()
 
-        result = link_checker_results.get("test_resource_id")
+        result = results.get("test_resource_id")
 
         assert result["num_fails"] == 3
         assert result["last_checked"] > before
@@ -105,13 +105,13 @@ class TestUpsertAndGet(object):
     def test_reset_num_fails(self):
         """Test that a successful result resets num_fails to 0."""
 
-        link_checker_results.upsert("test_resource_id", False)
-        link_checker_results.upsert("test_resource_id", False)
+        results.upsert("test_resource_id", False)
+        results.upsert("test_resource_id", False)
         before = datetime.datetime.utcnow()
-        link_checker_results.upsert("test_resource_id", True)
+        results.upsert("test_resource_id", True)
         after = datetime.datetime.utcnow()
 
-        result = link_checker_results.get("test_resource_id")
+        result = results.get("test_resource_id")
 
         assert result["num_fails"] == 0
         assert result["last_checked"] > before
@@ -126,16 +126,16 @@ class TestUpsertAndGet(object):
         """
         import ckan.model
 
-        result = link_checker_results._LinkCheckerResult(
+        result = results._LinkCheckerResult(
             "test_resource_id", None, pending=True)
         result.pending = True
         result.pending_since
         ckan.model.Session.add(result)
         ckan.model.Session.commit()
 
-        link_checker_results.upsert("test_resource_id", True)
+        results.upsert("test_resource_id", True)
 
-        result = link_checker_results.get("test_resource_id")
+        result = results.get("test_resource_id")
 
         assert result["pending"] is False
         assert result["pending_since"] is None
@@ -144,8 +144,8 @@ class TestUpsertAndGet(object):
         """get() should raise NoResultForResourceError if asked for the result
         for a resource ID that has no results."""
 
-        nose.tools.assert_raises(link_checker_results.NoResultForResourceError,
-                                 link_checker_results.get, "test_resource_id")
+        nose.tools.assert_raises(results.NoResultForResourceError,
+                                 results.get, "test_resource_id")
 
 
 class TestGetResourcesToCheck(object):
@@ -153,7 +153,7 @@ class TestGetResourcesToCheck(object):
 
     def setup(self):
         helpers.reset_db()
-        link_checker_results.create_database_table()
+        results.create_database_table()
 
     def test_with_5_new_resources_and_request_10(self):
         """
@@ -168,7 +168,7 @@ class TestGetResourcesToCheck(object):
         resource_4 = factories.Resource()['id']
         resource_5 = factories.Resource()['id']
 
-        resources_to_check = link_checker_results.get_resources_to_check(10)
+        resources_to_check = results.get_resources_to_check(10)
 
         assert resources_to_check == [resource_1, resource_2, resource_3,
                                       resource_4, resource_5]
@@ -191,7 +191,7 @@ class TestGetResourcesToCheck(object):
         factories.Resource()['id']
         factories.Resource()['id']
 
-        resources_to_check = link_checker_results.get_resources_to_check(5)
+        resources_to_check = results.get_resources_to_check(5)
 
         assert resources_to_check == [resource_1, resource_2, resource_3,
                                       resource_4, resource_5]
@@ -210,13 +210,13 @@ class TestGetResourcesToCheck(object):
         resource_5 = factories.Resource()['id']
         twenty_hours_ago = datetime.datetime.utcnow() - datetime.timedelta(
             hours=23)
-        link_checker_results.upsert(resource_1, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_2, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_3, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_4, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_5, True, twenty_hours_ago)
+        results.upsert(resource_1, True, twenty_hours_ago)
+        results.upsert(resource_2, True, twenty_hours_ago)
+        results.upsert(resource_3, True, twenty_hours_ago)
+        results.upsert(resource_4, True, twenty_hours_ago)
+        results.upsert(resource_5, True, twenty_hours_ago)
 
-        resources_to_check = link_checker_results.get_resources_to_check(10)
+        resources_to_check = results.get_resources_to_check(10)
 
         assert resources_to_check == []
 
@@ -235,18 +235,18 @@ class TestGetResourcesToCheck(object):
         resource_5 = factories.Resource()['id']
         twenty_hours_ago = datetime.datetime.utcnow() - datetime.timedelta(
             hours=23)
-        link_checker_results.upsert(resource_1, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_2, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_3, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_4, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_5, True, twenty_hours_ago)
+        results.upsert(resource_1, True, twenty_hours_ago)
+        results.upsert(resource_2, True, twenty_hours_ago)
+        results.upsert(resource_3, True, twenty_hours_ago)
+        results.upsert(resource_4, True, twenty_hours_ago)
+        results.upsert(resource_5, True, twenty_hours_ago)
         resource_6 = factories.Resource()['id']
         resource_7 = factories.Resource()['id']
         resource_8 = factories.Resource()['id']
         resource_9 = factories.Resource()['id']
         resource_10 = factories.Resource()['id']
 
-        resources_to_check = link_checker_results.get_resources_to_check(10)
+        resources_to_check = results.get_resources_to_check(10)
 
         assert resources_to_check == [resource_6, resource_7, resource_8,
                                       resource_9, resource_10]
@@ -267,29 +267,29 @@ class TestGetResourcesToCheck(object):
         resource_4 = factories.Resource()['id']
         resource_5 = factories.Resource()['id']
         twenty_hours_ago = now - datetime.timedelta(hours=23)
-        link_checker_results.upsert(resource_1, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_2, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_3, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_4, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_5, True, twenty_hours_ago)
+        results.upsert(resource_1, True, twenty_hours_ago)
+        results.upsert(resource_2, True, twenty_hours_ago)
+        results.upsert(resource_3, True, twenty_hours_ago)
+        results.upsert(resource_4, True, twenty_hours_ago)
+        results.upsert(resource_5, True, twenty_hours_ago)
         resource_6 = factories.Resource()['id']
         resource_7 = factories.Resource()['id']
         resource_8 = factories.Resource()['id']
         resource_9 = factories.Resource()['id']
         resource_10 = factories.Resource()['id']
         # We mix up the order in which these resources were checked a bit.
-        link_checker_results.upsert(
+        results.upsert(
             resource_7, True, now - datetime.timedelta(hours=34))
-        link_checker_results.upsert(
+        results.upsert(
             resource_6, True, now - datetime.timedelta(hours=33))
-        link_checker_results.upsert(
+        results.upsert(
             resource_9, True, now - datetime.timedelta(hours=32))
-        link_checker_results.upsert(
+        results.upsert(
             resource_10, True, now - datetime.timedelta(hours=31))
-        link_checker_results.upsert(
+        results.upsert(
             resource_8, True, now - datetime.timedelta(hours=30))
 
-        resources_to_check = link_checker_results.get_resources_to_check(10)
+        resources_to_check = results.get_resources_to_check(10)
 
         assert resources_to_check == [resource_7, resource_6, resource_9,
                                       resource_10, resource_8]
@@ -307,11 +307,11 @@ class TestGetResourcesToCheck(object):
         resource_4 = factories.Resource()['id']
         resource_5 = factories.Resource()['id']
         twenty_hours_ago = now - datetime.timedelta(hours=20)
-        link_checker_results.upsert(resource_1, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_2, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_3, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_4, True, twenty_hours_ago)
-        link_checker_results.upsert(resource_5, True, twenty_hours_ago)
+        results.upsert(resource_1, True, twenty_hours_ago)
+        results.upsert(resource_2, True, twenty_hours_ago)
+        results.upsert(resource_3, True, twenty_hours_ago)
+        results.upsert(resource_4, True, twenty_hours_ago)
+        results.upsert(resource_5, True, twenty_hours_ago)
 
         # Create 5 resources with pending checks from < 2 hours ago.
         resource_6 = factories.Resource()['id']
@@ -320,7 +320,7 @@ class TestGetResourcesToCheck(object):
         resource_9 = factories.Resource()['id']
         resource_10 = factories.Resource()['id']
         one_hour_ago = now - datetime.timedelta(hours=1)
-        link_checker_results._make_pending(
+        results._make_pending(
             [resource_6, resource_7, resource_8, resource_9, resource_10],
             one_hour_ago)
 
@@ -330,18 +330,13 @@ class TestGetResourcesToCheck(object):
         resource_13 = factories.Resource()['id']
         resource_14 = factories.Resource()['id']
         resource_15 = factories.Resource()['id']
-        link_checker_results.upsert(resource_11, True,
-                                    now - datetime.timedelta(hours=35))
-        link_checker_results.upsert(resource_12, True,
-                                    now - datetime.timedelta(hours=34))
-        link_checker_results.upsert(resource_13, True,
-                                    now - datetime.timedelta(hours=33))
-        link_checker_results.upsert(resource_14, True,
-                                    now - datetime.timedelta(hours=32))
-        link_checker_results.upsert(resource_15, True,
-                                    now - datetime.timedelta(hours=31))
+        results.upsert(resource_11, True, now - datetime.timedelta(hours=35))
+        results.upsert(resource_12, True, now - datetime.timedelta(hours=34))
+        results.upsert(resource_13, True, now - datetime.timedelta(hours=33))
+        results.upsert(resource_14, True, now - datetime.timedelta(hours=32))
+        results.upsert(resource_15, True, now - datetime.timedelta(hours=31))
 
-        resources_to_check = link_checker_results.get_resources_to_check(10)
+        resources_to_check = results.get_resources_to_check(10)
 
         assert resources_to_check == [resource_11, resource_12, resource_13,
                                       resource_14, resource_15]
@@ -360,11 +355,11 @@ class TestGetResourcesToCheck(object):
         resource_5 = factories.Resource()['id']
         five_hours_ago = datetime.datetime.utcnow() - datetime.timedelta(
             hours=5)
-        link_checker_results._make_pending(
+        results._make_pending(
             [resource_1, resource_2, resource_3, resource_4, resource_5],
             five_hours_ago)
 
-        resources_to_check = link_checker_results.get_resources_to_check(10)
+        resources_to_check = results.get_resources_to_check(10)
 
         assert resources_to_check == [resource_1, resource_2, resource_3,
                                       resource_4, resource_5]
@@ -380,18 +375,18 @@ class TestGetResourcesToCheck(object):
         resource_2 = factories.Resource()['id']
         thirty_hours_ago = datetime.datetime.utcnow() - datetime.timedelta(
             hours=30)
-        link_checker_results.upsert(resource_2, True, thirty_hours_ago)
+        results.upsert(resource_2, True, thirty_hours_ago)
 
         # A resource with a pending check from > 2 hours ago.
         resource_3 = factories.Resource()['id']
         three_hours_ago = datetime.datetime.utcnow() - datetime.timedelta(
             hours=3)
-        link_checker_results._make_pending([resource_3], three_hours_ago)
+        results._make_pending([resource_3], three_hours_ago)
 
-        link_checker_results.get_resources_to_check(10)
+        results.get_resources_to_check(10)
 
         for resource in (resource_1, resource_2, resource_3):
-            result = link_checker_results.get(resource)
+            result = results.get(resource)
             assert result["pending"] is True
 
     def test_custom_shorter_since(self):
@@ -400,13 +395,13 @@ class TestGetResourcesToCheck(object):
         test_resource = factories.Resource()['id']
         ten_hours_ago = datetime.datetime.utcnow() - datetime.timedelta(
             hours=10)
-        link_checker_results.upsert(test_resource, True, ten_hours_ago)
+        results.upsert(test_resource, True, ten_hours_ago)
 
-        results = link_checker_results.get_resources_to_check(
+        results_ = results.get_resources_to_check(
             10, since=datetime.timedelta(hours=5))
 
-        assert len(results) == 1
-        assert results[0] == test_resource
+        assert len(results_) == 1
+        assert results_[0] == test_resource
 
     def test_custom_longer_since(self):
         """If given a longer ``since`` time it should not return resources that
@@ -414,12 +409,12 @@ class TestGetResourcesToCheck(object):
         test_resource = factories.Resource()['id']
         thirty_hours_ago = datetime.datetime.utcnow() - datetime.timedelta(
             hours=30)
-        link_checker_results.upsert(test_resource, True, thirty_hours_ago)
+        results.upsert(test_resource, True, thirty_hours_ago)
 
-        results = link_checker_results.get_resources_to_check(
+        results_ = results.get_resources_to_check(
             10, since=datetime.timedelta(hours=48))
 
-        assert results == []
+        assert results_ == []
 
     def test_custom_shorter_pending_since(self):
         """If given a shorter ``pending_since`` time it should return
@@ -427,13 +422,13 @@ class TestGetResourcesToCheck(object):
         test_resource = factories.Resource()['id']
         one_hour_ago = datetime.datetime.utcnow() - datetime.timedelta(
             hours=1)
-        link_checker_results._make_pending([test_resource], one_hour_ago)
+        results._make_pending([test_resource], one_hour_ago)
 
-        results = link_checker_results.get_resources_to_check(
+        results_ = results.get_resources_to_check(
             10, pending_since=datetime.timedelta(hours=0.5))
 
-        assert len(results) == 1
-        assert results[0] == test_resource
+        assert len(results_) == 1
+        assert results_[0] == test_resource
 
     def test_custom_longer_pending_since(self):
         """If given a longer ``pending_since`` time it should not return
@@ -441,9 +436,9 @@ class TestGetResourcesToCheck(object):
         test_resource = factories.Resource()['id']
         three_hours_ago = datetime.datetime.utcnow() - datetime.timedelta(
             hours=3)
-        link_checker_results._make_pending([test_resource], three_hours_ago)
+        results._make_pending([test_resource], three_hours_ago)
 
-        results = link_checker_results.get_resources_to_check(
+        results_ = results.get_resources_to_check(
             10, pending_since=datetime.timedelta(hours=4))
 
-        assert results == []
+        assert results_ == []
