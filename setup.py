212cafd5934ac8186022822a5591deef9e70c2b8
Sean Hammond <git@seanh.cc>
2014-10-13 22:54:47 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.1.5',
+    version='0.1.6',
 
     description='A CKAN extension for the Dead or Alive link checker service',
     long_description=long_description,
